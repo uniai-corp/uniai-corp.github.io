333724c910a3c343b4df4d3b9440eaf5672f95a2
graffitoRyu <yth4135@naver.com>
2024-05-29 10:39:39 +0900

    [FEATURE] 인터랙션 모션 동작 완료

--- src/components/section/Title.tsx
+++ src/components/section/Title.tsx
@@ -30,6 +30,7 @@ export default function PageSectionHeaderTitleContainer({
             duration: 0.48,
             autoAlpha: 1,
             skewX: -30,
+            delay: 0.16,
             scrollTrigger: {
               trigger: titleContainerRef.current,
               start: "top 60%",
@@ -45,7 +46,7 @@ export default function PageSectionHeaderTitleContainer({
           {
             duration: 0.64,
             autoAlpha: 1,
-            delay: 0.24,
+            translateY: 0,
             scrollTrigger: {
               trigger: titleContainerRef.current,
               start: "top 60%",
@@ -59,10 +60,10 @@ export default function PageSectionHeaderTitleContainer({
         target: titleContainerRef.current?.querySelector(".page-section-header-subtitle") || null,
         animation: [
           {
-            duration: 0.64,
+            duration: 0.8,
             autoAlpha: 1,
             translateY: 0,
-            delay: 0.48,
+            delay: 0.64,
             scrollTrigger: {
               trigger: titleContainerRef.current,
               start: "top 60%",
