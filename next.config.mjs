cf0c427b7aaffd784ad6b7e8febb682610a457a2
GraffitoRyu <yth4135@naver.com>
2024-09-05 10:58:18 +0900

    [CHORE] 패키지 업데이트 및 환경설정 업데이트 - next.js 타입스크립트 관련 eslint 환경 설정 방법 업데이트 - 설정파일을 js에서 mjs로 변경 (최신 create-next-app 설정파일 구조 반영)

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+
+import path from "path";
+
+const root = path.dirname(new URL(import.meta.url).pathname);
+
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  optimizeFonts: false,
+  sassOptions: {
+    includesPaths: [path.join(root, "styles")],
+  },
+};
+
+export default nextConfig;
