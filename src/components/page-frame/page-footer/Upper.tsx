435ef02920333e9509b37a12ee0f3e5d5efa96f1
GraffitoRyu <yth4135@naver.com>
2025-08-17 10:46:28 +0900

    [DESIGN] 페이지 푸터 리뉴얼 완료

--- src/components/page-frame/page-footer/Upper.tsx
+++ src/components/page-frame/page-footer/Upper.tsx
@@ -1,29 +1,8 @@
 "use client";
-// import { getImageProps } from "next/image";
 
 import useLocale from "@/hooks/useLocale";
 
 export default function PageFooterUpper() {
-  // const common = { alt: "UNiAI" };
-
-  // const {
-  //   props: { srcSet: desktop },
-  // } = getImageProps({
-  //   ...common,
-  //   width: 158,
-  //   height: 44,
-  //   src: "/img/common/logo_footer_desktop.png",
-  // });
-
-  // const {
-  //   props: { srcSet: mobile, ...rest },
-  // } = getImageProps({
-  //   ...common,
-  //   width: 120,
-  //   height: 34,
-  //   src: "/img/common/logo_footer_mobile.png",
-  // });
-
   const { t } = useLocale();
 
   return (
@@ -31,12 +10,7 @@ export default function PageFooterUpper() {
       <div className="footer-upper-item logo-box">
         <figure>
           <picture>
-            <source media="(min-width: 1280px)" srcSet="/img/common/logo_footer_desktop.png" />
-            <img
-              src="/img/common/logo_footer_mobile.png"
-              alt="UNiAI"
-              style={{ width: "100%", height: "auto" }}
-            />
+            <img src="/img/logo/logo-footer.svg" alt="UNiAI" />
           </picture>
         </figure>
         <figcaption>{t("common/company/name", "주식회사 유니아이")}</figcaption>
