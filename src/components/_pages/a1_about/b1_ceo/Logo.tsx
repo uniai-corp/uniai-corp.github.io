13f60698a1237672e83f2b5ea69fa8b91288f744
GraffitoRyu <yth4135@naver.com>
2025-08-17 10:52:15 +0900

    [DESIGN] 로고 퀄리티 개선 및 교체

--- src/components/_pages/a1_about/b1_ceo/Logo.tsx
+++ src/components/_pages/a1_about/b1_ceo/Logo.tsx
@@ -1,36 +1,10 @@
-// import { getImageProps } from "next/image";
 import { forwardRef } from "react";
 
 const PageAboutCEOHeaderLogo = forwardRef((props, ref: React.ForwardedRef<HTMLElement | null>) => {
-  // const commonLogo = { alt: "유니아이" };
-
-  // const {
-  //   props: { srcSet: desktopLogo },
-  // } = getImageProps({
-  //   ...commonLogo,
-  //   width: 248,
-  //   height: 69,
-  //   src: "/img/about/ceo/logo_desktop.png",
-  // });
-
-  // const {
-  //   props: { srcSet: mobileLogo, ...restLogo },
-  // } = getImageProps({
-  //   ...commonLogo,
-  //   width: 166,
-  //   height: 46,
-  //   src: "/img/about/ceo/logo_mobile.png",
-  // });
-
   return (
     <figure ref={ref}>
       <picture>
-        <source media="(min-width: 1280px)" srcSet="/img/about/ceo/logo_desktop.png" />
-        <img
-          src="/img/about/ceo/logo_mobile.png"
-          alt="UNiAI"
-          style={{ width: "100%", height: "auto" }}
-        />
+        <img src="/img/logo/logo-about.svg" alt="UNiAI" />
       </picture>
     </figure>
   );
--- src/components/page-frame/page-header/Logo.tsx
+++ src/components/page-frame/page-header/Logo.tsx
@@ -1,28 +1,16 @@
 "use client";
 
 import Link from "next/link";
-import { useMemo } from "react";
-import { useAtomValue } from "jotai";
-
 import useLocale from "@/hooks/useLocale";
-
-import { responsiveDeviceState } from "@/jotai/device-viewport";
 
 export default function PageHeaderLogo() {
   const { locale } = useLocale();
-  const { viewport } = useAtomValue(responsiveDeviceState);
-  const style = useMemo(
-    () => (viewport === "desktop" ? { width: "100%", height: "auto" } : {}),
-    [viewport],
-  );
-
   return (
     <h1 className="page-header-logo">
       <Link href={`/${locale === "ko" ? "" : locale}`}>
         <figure>
           <picture>
-            <source media="(min-width: 768px)" srcSet="/img/logo/logo-header-desktop.png" />
-            <img src="/img/logo/logo-header-mobile.png" alt="UNiAI" style={style} />
+            <img src="/img/logo/logo-header.svg" alt="UNiAI" />
           </picture>
         </figure>
       </Link>
