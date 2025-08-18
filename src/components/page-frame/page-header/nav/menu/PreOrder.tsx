a4b4967ef0c125bf1af075da9d70f90dbf1741c6
GraffitoRyu <yth4135@naver.com>
2025-08-18 16:49:56 +0900

    [FIX] 사전구매 버튼 다국어처리

--- src/components/page-frame/page-header/nav/menu/PreOrder.tsx
+++ src/components/page-frame/page-header/nav/menu/PreOrder.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 // import Link from "next/link";
 
+import useLocale from "@/hooks/useLocale";
+
 export default function PageNavPreOrderButton() {
+  const { t } = useLocale();
+
   return (
     <button
       type="button"
       className="page-nav-pre-order-button"
       onClick={() => alert("페이지 준비중입니다.")}
     >
-      <span>V2 사전구매</span>
+      <span>{t("page/pre-order", "V2 사전구매")}</span>
     </button>
   );
 }
