3f1ba28e1c27bd25ffc8a6c2318b3ebe0ad72208
GraffitoRyu <yth4135@naver.com>
2025-08-18 13:18:29 +0900

    [FIX] 사전구매 페이지 접근방지

--- src/components/page-frame/page-header/nav/menu/PreOrder.tsx
+++ src/components/page-frame/page-header/nav/menu/PreOrder.tsx
@@ -1,9 +1,13 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function PageNavPreOrderButton() {
   return (
-    <Link className="page-nav-pre-order-button" href="/pre-order">
+    <button
+      type="button"
+      className="page-nav-pre-order-button"
+      onClick={() => alert("페이지 준비중입니다.")}
+    >
       <span>V2 사전구매</span>
-    </Link>
+    </button>
   );
 }
