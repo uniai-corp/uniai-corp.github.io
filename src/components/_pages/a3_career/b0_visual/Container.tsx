088da18bc00dd26fcc57298bbfa9970f791f6d40
GraffitoRyu <yth4135@naver.com>
2025-09-06 18:24:23 +0900

    [FEATURE] 리뉴얼 서브비주얼 구조 추상화

--- src/components/_pages/a3_career/b0_visual/Container.tsx
+++ src/components/_pages/a3_career/b0_visual/Container.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { useRef } from "react";
-
-import PageSectionContainer from "@/components/section/Container";
-import PageSectionWrapper from "@/components/section/Wrapper";
+import PageSubVisual from "@/components/page-frame/SubVisual";
 
 export default function PageCareerVisual() {
   const sectionRef = useRef<HTMLElement>(null);
@@ -11,28 +9,15 @@ export default function PageCareerVisual() {
   const descriptionRef = useRef<HTMLParagraphElement>(null);
 
   return (
-    <PageSectionContainer className="page-sub-visual page-career-visual" ref={sectionRef}>
-      <figure className="bg">
-        <picture>
-          <img
-            src="/renew/career/sub-visual.jpg"
-            alt="인재채용 비주얼"
-            style={{ width: "100%", height: "100%" }}
-          />
-        </picture>
+    <PageSubVisual className="page-career-visual" ref={sectionRef}>
+      <figure className="visual-tag">
+        <span>Now We{`'`}re Hiring</span>
       </figure>
-      <div className="cover">
-        <PageSectionWrapper>
-          <figure className="visual-tag">
-            <span>Now We{`'`}re Hiring</span>
-          </figure>
-          <h2 ref={titleRef}>
-            <span>2025</span>
-            <span>유니아이 인재채용</span>
-          </h2>
-          <p ref={descriptionRef}>Looking forward to growing together</p>
-        </PageSectionWrapper>
-      </div>
-    </PageSectionContainer>
+      <h2 ref={titleRef}>
+        <span>2025</span>
+        <span>유니아이 인재채용</span>
+      </h2>
+      <p ref={descriptionRef}>Looking forward to growing together</p>
+    </PageSubVisual>
   );
 }
