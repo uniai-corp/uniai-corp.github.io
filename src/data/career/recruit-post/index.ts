b9cb6b1a409fed518258b8460c487d3b5ba968f5
GraffitoRyu <yth4135@naver.com>
2025-11-24 13:41:41 +0900

    [Fix] 채용공고 종료

--- src/data/career/recruit-post/index.ts
+++ src/data/career/recruit-post/index.ts
@@ -1,9 +1,9 @@
 // import { recruit_fe_engineer } from "./fe-engineer";
-import { recruit_be_engineer } from "./be-engineer";
+// import { recruit_be_engineer } from "./be-engineer";
 
 const recruit: Record<string, RecruitPost> = {
   // fe_engineer: recruit_fe_engineer,
-  be_engineer: recruit_be_engineer,
+  // be_engineer: recruit_be_engineer,
 };
 
 export default recruit;
